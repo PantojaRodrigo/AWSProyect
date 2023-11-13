ebe3d5a3b309a1e78ae156cf9607114ee035ce94
PantojaRodrigo <rodrigopanv@outlook.com>
2023-11-12 18:12:31 -0600

    primera entrega

---
+++ repository.py
@@ -0,0 +1,30 @@
+from api.models.alumno import Alumno
+from api.models.profesor import Profesor
+
+
+class Repository():
+    elementos :list[Alumno|Profesor]= []
+          
+    def add_element(self,element:Alumno|Profesor):
+        if self.get_element_by_id(element.id) is None: 
+            self.elementos.append(element)
+            return element.id
+        return None
+        
+    def get_element_by_id(self, id:int)->(Alumno|Profesor|None):
+        return next(filter(lambda element: element.id == id,  self.elementos), None)
+    
+    def update_element(self,id:int,new_element:Alumno|Profesor):
+        element = self.get_element_by_id(id) 
+        if element is not None and element.id==new_element.id:
+            self.elementos.remove(element)
+            self.elementos.append(new_element)
+            return new_element.id 
+        return None
+    
+    def delete_element(self,id:int):
+        element = self.get_element_by_id(id) 
+        if element is not None :
+            self.elementos.remove(element)
+            return True
+        return False
