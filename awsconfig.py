e408b166e2bb2fd090d10688a2cc70d49f8661d4
PantojaRodrigo <rodrigopanv@outlook.com>
2023-11-27 23:39:41 -0600

    update awsconfig

--- awsconfig.py
+++ awsconfig.py
@@ -1,5 +1,6 @@
 import boto3
 session = boto3.Session(
+    region_name='us-east-1',
     aws_access_key_id="ASIAZOIVOVX5UGLMJUOG",
     aws_secret_access_key="ClMv8P0MjXJeg5h476+YpqiiSS+4AdBDpMZVY2d8",
     aws_session_token="FwoGZXIvYXdzEGsaDAUuFDxuG0lrIWyjpSLQAVQm+WrpfMV8wI8LrWCEimXLb4QNc/4Ueo1I+lAa5vp6vEG9oZwNQIxL4vHtP+5NI4a4CSCQHqWjnRNu6M9A0/O6hsDvFqEUXbXsndzDUn9QRVh3USaLArpIPEiOuUhT/0PpHu4MNJMLDGCZpqzYURLi5DZgC84cQcRmkGWVXPcWhglKwpMmX3iu0Je6BBUNziE5CaI44chsYv03SOx3Q4OnW7LHEA1yDVpP9Ue08kqSu0G4NRlVGUk6chR1Kh+DUhX4/H3wA6Pgvd+QHLDI1ooonYeVqwYyLdAEzqZQnzDf9xg3PVimcDBG01ilA25uFGe689KOVWl7brV/qhrReNWmx7HUPw=="
