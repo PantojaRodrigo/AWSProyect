aa73407ea4a35d7823516b0fde729d14b465d0eb
PantojaRodrigo <rodrigopanv@outlook.com>
2023-12-12 17:53:10 -0600

    update credentials

--- awsconfig.py
+++ awsconfig.py
@@ -1,9 +1,9 @@
 import boto3
 session = boto3.Session(
     region_name='us-east-1',
-    aws_access_key_id="ASIAZOIVOVX55PF4BZIS",
-    aws_secret_access_key="exs1FckwAe5jxMaGZ7ZmM7Ge+S31QE1CLErhOjZo",
-    aws_session_token="FwoGZXIvYXdzEG8aDITukhyOjMgksNYILiLQAQEkCbTifnAQ28fewxHmrnDEDRQGIGcb/zFFYvGEHJa8l5gBTI809t33WPb+3yA7Mq9wsGCgXHKHDaWesPtP/aXITVqZqbVe7xEq3F64NIzi3kPiMc1Gr78OqAhzhfBccKke7ZH/HP4ZzB+EHmFVD9uVaYLJnallB6AA73A+yr1kf340OFaPnTFZan2o0uGZqf89blXMetF3W93jrcr24WfOcZ6SDBHuIpSP55hTF5EeLSAcn4b4cWMMxtnXUa0LP04NjWcmUcYJF1Vc6VTPw7AoxvqVqwYyLfabL9Ri9tfBytZ4G/guwboYGnJN29e+O/W2kZG2vt/bBC/IgaVHWgOFMbZlrQ=="
+    aws_access_key_id="ASIAZOIVOVX55DZBFLXS",
+    aws_secret_access_key="9KVx2+a9g4iaQ7g1MtXHuBIuBa63hvFXfUwCQR9z",
+    aws_session_token="FwoGZXIvYXdzENH//////////wEaDGvzQ7CJqksiq4/PViLQASR3T4ppODVSeGUUTu9+HX94BVACWqNsMbnIQgx4Goim2aIHKx2jLKEIFMuV5YwYmR0NalcYg0VKBMwaqj8JdwbrMz0vrSNE5wG6/pxO2nypHF81b1jabbebtIE9hJsebqW69CEAOPZ1sM6Bx8VbXoR4bNMUMxhSrEaju64mMEVIDM5GR1UQVhtmpAi8qBnIYGFwlMwcYrbZ/jsW3Aev0f0lddlqX3OXay7XMqSRP77PUoKRFl96FOGr04/pe6BfYyxurLgjrT4UFEXcHW7XZhEo2NrjqwYyLXM6STAoHXBoxV6qi52snZfCCjG86BaHJaeeh6Gbiy9IjbdnSCEXdEBM2wObhA=="
 )
 
 s3_client = session.client('s3')
--- dbconnection.py
+++ dbconnection.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from  sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQALCHEMY_DATABASE_URL="mysql+mysqlconnector://admin:Admin123@db-proyect-aws.c10b9kpab8at.us-east-1.rds.amazonaws.com:3306/proyectodb"
+SQALCHEMY_DATABASE_URL="mysql+mysqlconnector://admin:Admin123@\
+                        db-proyect-aws.c10b9kpab8at.us-east-1.rds.amazonaws.com:3306\
+                        /proyectodb"
 
 
 engine = create_engine(SQALCHEMY_DATABASE_URL,echo=False)
